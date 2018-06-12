7f83db063a227fabe15d5fd93d326357cb52e756
Mandy Choi <hello@mandychoi.com.au>
2018-06-12 15:39:03 +0100

    Hello World node js app with docker

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # demo-nodejs-app
+
+
+To build
+docker build -t demo-nodejs-app .
+
+To run
+docker run -p 18080:8080 -d demo-nodejs-app
+
+To go into container
+docker exec -it <container id> /bin/bash
---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mchoi on 12/06/2018.
+ */
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
