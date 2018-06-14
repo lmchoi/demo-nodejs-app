d25fd771b07fd0318deb39d9f0bd74de480750bc
Mandy Choi <hello@mandychoi.com.au>
2018-06-14 17:32:49 +0100

    add mongoose support, print first record

--- src/server.js
+++ src/server.js
@@ -3,16 +3,24 @@
  */
 'use strict'
 import express from 'express'
+import mongoose from 'mongoose'
 import routes from './app/routes'
+import * as db from './config/db'
 
 // Constants
 const PORT = 8080
 const HOST = '0.0.0.0'
 
-// App
-const app = express()
+// datatstore ==============================================
+var connectionsubject = mongoose.createConnection(db.urlSubjectViews)
+var Subjects = connectionsubject.model('', {}, 'projects');
+Subjects.find({'_projectid': 1}, function (err, subject) {
+  if (err) return console.error(err);
+  console.log(subject + '\n')
+})
 
 // routes ==================================================
+const app = express()
 routes(app) // pass our application into our routes
 
 // start app ===============================================
